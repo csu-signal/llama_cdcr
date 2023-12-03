dc34074a520c5be8e58b3f015b97a19c7da520d1
Abhijnan Nath <abhijnan@carnap.cs.colostate.edu>
2023-12-03 14:21:10 -0700

    llama 7b chat eval zero and few shot

--- example_chat_completion_falcon.py
+++ example_chat_completion_falcon.py
@@ -10,10 +10,10 @@ import spacy
 import pickle
 from collections import defaultdict
 output_list = []
-dataset = 'gvc'
+dataset = 'ecb'
 generated_IM_samples = defaultdict(list)
 
-output_folder = f'/s/chopin/d/proj/ramfis-aida/llama2/llama/{dataset}/'
+output_folder = f'/s/chopin/d/proj/ramfis-aida/llama2/llama/datasets/{dataset}/'
 def create_inner_monologue_prompts(dataset_folder =None):
 
     dataset_folder = f'/s/chopin/d/proj/ramfis-aida/multimodal_NLI/Multimodal_CDCR/acl_submission_2023-main/datasets/{dataset}/'
@@ -45,8 +45,8 @@ def create_inner_monologue_prompts(dataset_folder =None):
     test_pairs_lh = list(tps_test + fps_test)
     len(train_pairs_lh), len(dev_pairs_lh), len(test_pairs_lh)
 
-    train_pairs_lh = train_pairs_lh[15000:35000] #get the first 20000, then get the rest.
-    train_labels = train_labels[15000:35000]
+    train_pairs_lh = train_pairs_lh[35000:len(train_pairs_lh)] #get the first 20000, then get the rest. Then 15000 to 35000. Last, 35000:len(train_pairs_lh) for GVC
+    train_labels = train_labels[35000:len(train_labels)]
     print(f"Generating Inner Monologue Samples for {len(train_pairs_lh), len(train_labels)} samples")
 
     prompt_list = []
@@ -129,6 +129,90 @@ def create_inner_monologue_prompts(dataset_folder =None):
             dialog_list.append(final_dialog)
 
     return dialog_list,train_pairs_lh, train_labels
+
+
+def create_zero_shot_prompts_for_eval(dataset):
+    print(f"getting eval results for dataset:{dataset}")
+    dataset_folder = f'/s/chopin/d/proj/ramfis-aida/multimodal_NLI/Multimodal_CDCR/acl_submission_2023-main/datasets/{dataset}/'
+    mention_map = pickle.load(open(dataset_folder + "/mention_map.pkl", 'rb'))
+    evt_mention_map = {m_id: m for m_id, m in mention_map.items() if m['men_type'] == 'evt' and m['split'] == 'test'}
+    test_mp_mpt, _ = pickle.load(open(dataset_folder + '/lh_llama/mp_mp_t_test.pkl', 'rb'))
+    tps_test, fps_test, _, _ = test_mp_mpt
+    test_pairs_lh = list(tps_test + fps_test)
+    test_labels = [1] * len(tps_test) + [0] * len(fps_test)
+    # test_pairs_lh = test_pairs_lh[0:100] #get the first 20000, then get the rest. Then 15000 to 35000. Last, 35000:len(train_pairs_lh) for GVC
+    # test_labels = test_labels[0:100]
+
+    print(f"Generating Inner Monologue Samples for {len(test_pairs_lh), len(test_labels)} samples")
+
+    prompt_list = []
+    dialog_list = []
+
+    def trim_mention_sentences(mention_map):
+        list_keys = []
+        nlp = spacy.load('en_core_web_sm')
+        for x, y in mention_map.items():
+            if len(mention_map[x]['bert_sentence'].split())>=300:
+                list_keys.append(x)
+                sentence = mention_map[x]['bert_sentence']
+                doc = nlp(sentence)
+                start_token_index = sentence.find('<m>')
+                end_token_index =  sentence.find('</m>')
+                word = doc.char_span(start_token_index+1,end_token_index-1)
+                # word will be None if the char span isn't valid
+                if word is not None:
+                    sent = word.sent
+                    new_bert_sentence = str(sent)
+                    mention_map[x]['bert_sentence'] = new_bert_sentence
+                    #print(x, new_bert_sentence)
+        return mention_map
+
+    evt_mention_map_train = trim_mention_sentences(evt_mention_map)
+    for i, x in enumerate(test_pairs_lh):
+
+        sentence_a = evt_mention_map[x[0]]['bert_sentence']
+        sentence_b = evt_mention_map[x[1]]['bert_sentence']
+        # sytem_prompt = f'''Think step by step. You are an event coreference resolution annotator and you have to answer\
+        # if two events marked with <m> and </m> tokens are coreferent given two sentences for context. \
+        # Answer "Yes" if they are coreferent and "No" if they are not coreferent.'''
+        system_prompt = f'''Think step by step. You are a coreference annotator and you \
+        have to make a decision about two events marked with <m> and </m> tokens. \
+        You are given two sentences. Answer in a few words if they are talking \
+        about the same event: that is, if they are coreferent. If you think they are different 
+        events and not coreferent, answer why they are different. HINT: To answer correctly, think critically if the names, places, location etc are the same.'''
+
+    #     system_prompt = f'''Think step by step and answer either yes or no. You are a coreference annotator and you \
+    #     have to make a decision about two events marked with <m> and </m> tokens. \
+    #     You are given two sentences. Answer "Yes" if they are the same event i,e., they are coreferent. Answer "No" if they are different events.
+    #     For example, 
+    #     Sentence 1:  The actress <m> rehired </m> longtime attorney Shawn Holley to handle her case. 
+    #     Sentence 2: Lindsay Lohan <m> rehires </m> longtime lawyer Shawn Holley , heads to rehab.
+    #     Answer: Yes. Coreferent.
+    #     Sentence 1: Video of Brooklyn woman's fatal shooting is played at trial of two men charged in rooftop <m> gunplay </m>.
+    #     Sentence 2: Video of Brooklyn woman's fatal shooting is <m> played </m> at trial of two men charged in rooftop gunplay.
+    #     Answer: No. Not Coreferent. They are different events.
+    #    '''
+
+        
+
+        user_prompt = f'''Sentence 1 is:{sentence_a}. Sentence 2 is: {sentence_b}
+        Your answer: 
+        '''
+        system_prompt = system_prompt.replace("\n", "")
+        system_prompt = "".join(system_prompt)
+         
+        system_dict = {
+                "role": "system",
+                "content": system_prompt,
+            }
+        user_dict = {"role": "user", "content": user_prompt}
+        final_dialog = [system_dict, user_dict]
+        dialog_list.append(final_dialog)
+
+       
+
+    return dialog_list,test_pairs_lh 
+
 def main(
     ckpt_dir: str,
     tokenizer_path: str,
@@ -136,7 +220,7 @@ def main(
     top_p: float = 0.9,
     max_seq_len: int = 4096,
     max_batch_size: int = 4,
-    max_gen_len: Optional[int] = None,
+    max_gen_len: Optional[int] = 512,
 ):
     generator = Llama.build(
         ckpt_dir=ckpt_dir,
@@ -144,8 +228,11 @@ def main(
         max_seq_len=max_seq_len,
         max_batch_size=max_batch_size,
     )
-    dialogs, train_pairs, train_labels = create_inner_monologue_prompts()
+    #dialogs, train_pairs, train_labels = create_inner_monologue_prompts()
+    dialogs, test_pairs  = create_zero_shot_prompts_for_eval(dataset = 'ecb')
     generated_IM_samples = {}
+    generated_eval_samples = {}
+     
 #     dialogs = [
 
 
@@ -190,6 +277,7 @@ def main(
 #         ],
 #     ]
     batch_size = 40
+
     if not os.path.exists(output_folder):
             os.makedirs(output_folder)
     for i in tqdm(range(0, len(dialogs), batch_size)):
@@ -198,7 +286,7 @@ def main(
 
         results = generator.chat_completion(
             batched_prompts,  # type: ignore
-            max_gen_len=max_gen_len,
+            max_gen_len=56, #keep max gen length to 5 tokens for zero-shot evaluation   
             temperature=temperature,
             top_p=top_p,
         )
@@ -206,21 +294,38 @@ def main(
         # generated_IM_samples[i].append(results)
         # generated_IM_samples = {x:y for x, y in zip(batched_pairs,results)}
 
-        if i%5000 == 0:
+        # if i%1000 == 0:  #save every 1k sample pairs 
 
-            with open(f'{output_folder}/result_dict_train_gvc_{i}_part2.pkl', 'wb') as fp:
+            # with open(f'{output_folder}/result_dict_train_gvc_{i}_part3.pkl', 'wb') as fp:
+            #     pickle.dump(output_list, fp)
+            # with open(f'{output_folder}/eval_results{i}.pkl', 'wb') as fp:
+            #     pickle.dump(output_list, fp)
+
+
+    with open(f'{output_folder}/eval_results_full_fewshot.pkl', 'wb') as fp:
                 pickle.dump(output_list, fp)
-    with open(f'{output_folder}/result_dict_train_gvc_full_part2.pkl', 'wb') as fp:
-                pickle.dump(output_list, fp)
 
-#     for dialog, result, pairs in zip(dialogs, results, train_labels):
-#         for msg in dialog:
+    # with open(f'{output_folder}/result_dict_train_gvc_full_part3.pkl', 'wb') as fp:
+    #             pickle.dump(output_list, fp)
 
-#             print(f"{msg['role'].capitalize()}: {msg['content']}\n")
-#         print(
-#             f"> {result['generation']['role'].capitalize()}: {result['generation']['content']}"
-#         )
-#         print("\n==================================\n")
+    # for dialog, result, pairs in zip(dialogs, results, train_labels):
+    #     for msg in dialog:
+
+    #         print(f"{msg['role'].capitalize()}: {msg['content']}\n")
+    #     print(
+    #         f"> {result['generation']['role'].capitalize()}: {result['generation']['content']}"
+    #     )
+    #     print("\n===================================\n")
+
+    for dialog, result in zip(dialogs, results):
+        for msg in dialog:
+
+            print(f"{msg['role'].capitalize()}: {msg['content']}\n")
+        print(
+            f"> {result['generation']['role'].capitalize()}: {result['generation']['content']}"
+        )
+        print("\n===================================\n")
+
     return results
 
 if __name__ == "__main__":
